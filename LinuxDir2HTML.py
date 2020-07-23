96f5ce2a7179e6719f7faf3e5e824f203248c089
Ali Homafar <home.isfar@gmail.com>
2020-07-22 21:23:39 -0400

    Initial commit

---
+++ LinuxDir2HTML.py
@@ -0,0 +1,145 @@
+# Licensed GPLv2
+# Copyright (C) ZapperDJ    2017 https://github.com/ZapperDJ/DiogenesList
+# Copyright (C) Ali Homafar 2020 https://github.com/homeisfar/LinuxDir2HTML
+
+import os
+import sys
+import datetime
+from os.path import getsize
+
+# global variables definition
+appName = "LinuxDir2HTML"
+appVer = "1.1"
+genDate = datetime.datetime.now().strftime("%m/%d/%Y")
+genTime = datetime.datetime.now().strftime("%H:%M")
+appLink = "https://github.com/homeisfar/LinuxDir2HTML"
+dir_results = []
+dirData = ""
+numFiles=0 
+numDirs=0 
+grandTotalSize=0
+linkFiles="false" # file linking not yet implemented
+
+# functions definition
+def generateDirArray(dirToScan):
+    global dirData
+    global numFiles
+    global numDirs
+    global grandTotalSize
+    global dir_results
+    # assign a number identifier to each directory
+    i = 1
+    dirIDsDictionary = {}
+    dirIDsDictionary[dirToScan] = 0
+    for currentDir, dirs, files in os.walk(dirToScan):
+        for dir in dirs:
+            full_dir_path = os.path.join(currentDir, dir)
+            if os.path.islink(full_dir_path):
+                continue
+            if os.path.isdir(full_dir_path):
+                dirIDsDictionary[full_dir_path] = i
+                i = i + 1
+    # Initialize array to hold all dir data, dimensioning it to hold the total number of dirs
+    allDirArray=[]
+    for p in range(i):
+        allDirArray.append(p)
+
+    # Traverse the directory tree
+    for currentDir, dirs, files in os.walk(dirToScan):
+        dirs = sorted(dirs, key=str.casefold)
+        files = sorted(files, key=str.casefold)
+        currentDirId=dirIDsDictionary[currentDir]
+        currentDirArray=[]
+        currentDirModifiedTime = datetime.datetime.fromtimestamp(os.path.getmtime(currentDir))
+        currentDirModifiedTime = currentDirModifiedTime.strftime("%m/%d/%Y %H:%M:%S")
+        currentDirFixed = currentDir.replace("/","\\\\")  # Replace / with \\ in the dir path (necessary for JavaScript functions to work properly)
+        currentDirArray.append(currentDirFixed+'*0*'+currentDirModifiedTime)
+        totalSize = 0
+        for file in files:
+            full_file_path = os.path.join(currentDir, file)
+            # Don't count symlinks or other oddities
+            if os.path.isfile(full_file_path):
+                numFiles = numFiles + 1
+                fileSize = getsize(full_file_path)
+                totalSize = totalSize + fileSize
+                grandTotalSize = grandTotalSize + fileSize
+                fileModifiedTime = datetime.datetime.fromtimestamp(os.path.getmtime(full_file_path))
+                fileModifiedTime = fileModifiedTime.strftime("%m/%d/%Y %H:%M:%S")
+                currentDirArray.append(f'{file}*{fileSize}*fileModifiedTime')
+        currentDirArray.append(totalSize)
+        # Create the list of directory IDs correspondent to the subdirs present on the current directory
+        # This acts as a list of links to the subdirectories on the JavaScript code
+        dirLinks = ''
+        for dir in dirs:
+            full_dir_path = os.path.join(currentDir, dir)
+            if os.path.islink(full_dir_path):
+                continue
+            numDirs = numDirs + 1
+            dirLinks = dirLinks + str(dirIDsDictionary[full_dir_path]) + '*'
+        dirLinks = dirLinks[:-1]    # remove last *
+        currentDirArray.append(dirLinks)
+        allDirArray[currentDirId]=currentDirArray
+
+    # from allDirArray, generate the text to replace [DIR DATA] on HTML file
+    #
+    # dirData format:
+    #
+    #   dirs[DIRECTORY_ID] =
+    #   "DIRECTORY_PATH*0*MODIFIED_TIME",
+    #   "FILENAME*FILESIZE*MODIFIED_TIME",
+    #   ...
+    #   TOTAL_FILE_SIZE,
+    #   "SUBDIRECTORY_ID*SUBDIRECTORY_ID*SUBDIRECTORY_ID*...",
+    #   ];
+
+    for d in range(len(allDirArray)):
+        dirData = f"dirs[{d}] = [\n"
+        for g in range(len(allDirArray[d])):
+            if type(allDirArray[d][g]) == int:
+                dirData += f"{allDirArray[d][g]},\n"
+            else:
+                dirData += f'"{allDirArray[d][g]}",\n'
+        dirData += "];\n\n"
+        dir_results.append(dirData)
+    return
+
+
+def generateHTML(dirData,appName,appVer,genDate,genTime,title,appLink,numFiles,numDirs,grandTotalSize,linkFiles):
+    templateFile = open(os.path.join(sys.path[0], 'template.html'), 'r')
+    outputFile = open(f'{title}.html', 'w')
+    for line in templateFile:
+        modifiedLine = line
+        if '[DIR DATA]' in modifiedLine:
+            for line in dir_results:
+                outputFile.write(line)
+            continue
+        modifiedLine = modifiedLine.replace('[APP NAME]', appName)
+        modifiedLine = modifiedLine.replace('[APP VER]', appVer)
+        modifiedLine = modifiedLine.replace('[GEN DATE]', genDate)
+        modifiedLine = modifiedLine.replace('[GEN TIME]', genTime)
+        modifiedLine = modifiedLine.replace('[TITLE]', title)
+        modifiedLine = modifiedLine.replace('[APP LINK]', appLink)
+        modifiedLine = modifiedLine.replace('[NUM FILES]', str(numFiles))
+        modifiedLine = modifiedLine.replace('[NUM DIRS]', str(numDirs))
+        modifiedLine = modifiedLine.replace('[TOT SIZE]', str(grandTotalSize))
+        modifiedLine = modifiedLine.replace('[LINK FILES]', linkFiles)
+        outputFile.write(modifiedLine)
+    templateFile.close()
+    outputFile.close()
+    print("Wrote output to: " + os.path.realpath(outputFile.name))
+
+# main program start point
+if len(sys.argv) < 3:
+    print("Missing arguments. This tool should be used as follows:")
+    print("     LinuxDir2HTML pathToIndex outputFileName")
+else:
+    pathToIndex = str(sys.argv[1])
+    title = str(sys.argv[2])
+    if os.path.exists(pathToIndex):
+        pathToIndex = os.path.abspath(pathToIndex)
+        print("Indexing directories...")
+        generateDirArray(pathToIndex)
+        print("Outputting HTML...")
+        generateHTML(dirData,appName,appVer,genDate,genTime,title,appLink,numFiles,numDirs,grandTotalSize,linkFiles)
+    else:
+        print("The specified directory doesn't exist. Aborting.")
---
+++ README.md
@@ -0,0 +1,35 @@
+# LinuxDir2HTML
+
+LinuxDir2HTML is a CLI-only clone of [Snap2HTML](https://www.rlvision.com/snap2html/). It is directly based on [DiogenesList](https://github.com/ZapperDJ/DiogenesList) but makes significant improvements.
+
+- Python 3.6+
+- Doesn't fail on symlinks (symlinks are ignored)
+- More graceful invocation and sane usage
+- Much, much, much faster
+
+This program will produce essentially an identical output to Snap2HTML - using the freely available template provided.
+
+## Usage
+The program only takes two arguments, the directory to be indexed and the output file name without the extension, so:
+
+ *python diogeneslist.py ~/Pictures output*
+ 
+Will index the contents of /home/Pictures and save them to the present working directory in an easy-to-use html file.
+
+Note this requires Python3.6 or greater, but if you modify the python to remove string interpolation the minimum version will be lower.
+
+## License
+The LinuxDir2HTML.py file is licensed under GPLv2.
+
+The template.html file is taken directly from Snap2HTML and is licensed as freeware.
+
+## Issues (Wishlist)
+The original Python code was not well-implemented. I've made essentially the minimum efforts needed to improve upon it, but have plans to continue making improvements, but no timeline to do so.
+
+The template HTML is based on an old version, and I'd like to update the output to use the latest available version from Snap2HTML. This will improve the search functionality, among other things.
+
+File links in the HTML are not implemented, and might never be.
+
+I'd also like to include an option to ignore hidden files.
+
+As an American, I prefer MM/DD/YYYY. Sorry!
